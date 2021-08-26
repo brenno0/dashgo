85985f37198686c0f9eb849575c90f1be138505e
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2021-08-26 20:38:06 -0300

    Implementando página home de SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,35 @@
 
-export default function Home() {
+import React from 'react'
+import { Flex,Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/input'
+
+export default function SignIn() {
   return (
-    <h1>Home</h1>
+    <Flex 
+      w="100vw"
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex 
+        as="form" 
+        width="100%" 
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+       
+          
+            <Input name="email" type="email" label="E-mail"  />
+            <Input name="email" type="password" label="Senha"  />
+
+        </Stack>
+        
+        <Button size="lg" type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
